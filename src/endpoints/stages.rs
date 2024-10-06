90014c7ca546fb42c9e36c0cdbcdacdfd80ae8f8
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2024-10-06 16:00:18 +0900

    Update stages.rs

--- src/endpoints/stages.rs
+++ src/endpoints/stages.rs
@@ -1,1 +1,26 @@
+use crate::client::SplaClient;
+use crate::models::stages::{BankaraOpenStagesResponse, Schedule};
+use reqwest::StatusCode;
 
+const BANKARA_OPEN_NEXT_ENDPOINT: &str = "https://spla3.yuu26.com/api/bankara-open/next";
+
+impl SplaClient {
+    pub async fn get_next_bankara_open_stages(&self) -> Result<Vec<Schedule>, Box<dyn std::error::Error>> {
+        let response = self.client
+            .get(BANKARA_OPEN_NEXT_ENDPOINT)
+            .send()
+            .await?;
+
+        println!("{:?}", response);
+
+        match response.status() {
+            StatusCode::OK => {
+                let bankara_open_stages: BankaraOpenStagesResponse = response.json().await?;
+                Ok(bankara_open_stages.results)
+            }
+            status => {
+                Err(format!("API request failed with status code: {}", status).into())
+            }
+        }
+    }
+}
