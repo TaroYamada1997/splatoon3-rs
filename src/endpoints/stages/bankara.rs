63d1696a9e3912ec9d574c0fc9aaab7301b24e98
Shota <149574372+TaroYamada1997@users.noreply.github.com>
2024-12-03 22:57:12 +0900

    feat: create get_bankara_challenge_schedule

--- src/endpoints/stages/bankara.rs
+++ src/endpoints/stages/bankara.rs
@@ -6,6 +6,7 @@ use crate::{
 };
 
 const BANKARA_OPEN_NEXT_ENDPOINT: &str = "bankara-open/next";
+const BANKARA_CHALLENGE_SCHEDULE_ENDPOINT: &str = "bankara-challenge/schedule";
 
 impl SplaClient {
     pub async fn get_next_bankara_open_stages(
@@ -23,6 +24,28 @@ impl SplaClient {
             StatusCode::OK => {
                 let bankara_open_stages: StagesResponse = response.json().await?;
                 Ok(bankara_open_stages.results)
+            }
+            status_code => {
+                Err(format!("API request failed with status code: {}", status_code).into())
+            }
+        }
+    }
+
+    pub async fn get_bankara_challenge_schedule(
+        &self,
+    ) -> Result<Vec<Schedule>, Box<dyn std::error::Error>> {
+        let base_url = &self.base_url;
+
+        let response = self
+            .client
+            .get(format!("{base_url}/{BANKARA_CHALLENGE_SCHEDULE_ENDPOINT}"))
+            .send()
+            .await?;
+
+        match response.status() {
+            StatusCode::OK => {
+                let bankara_challenge_stages: StagesResponse = response.json().await?;
+                Ok(bankara_challenge_stages.results)
             }
             status_code => {
                 Err(format!("API request failed with status code: {}", status_code).into())
